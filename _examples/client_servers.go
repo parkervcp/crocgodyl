2101a6f22ec465d34af12d895522950dc54181a3
devnote-dev <inferno0745@gmail.com>
2022-11-03 22:30:47 +0000

    feat(examples): replace client examples

---
+++ _examples/client_servers.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	croc "github.com/parkervcp/crocgodyl"
+)
+
+func main() {
+	client, _ := croc.NewClient(os.Getenv("CROC_URL"), os.Getenv("CROC_KEY"))
+
+	servers, err := client.GetServers()
+	if err != nil {
+		handleError(err)
+		return
+	}
+
+	for _, s := range servers {
+		fmt.Printf("%s (%d): %s\n", s.Identifier, s.InternalID, s.Name)
+	}
+
+	server := servers[0]
+	if server == nil {
+		return
+	}
+
+	if err = client.SetServerPowerState(server.Identifier, "restart"); err != nil {
+		handleError(err)
+		return
+	}
+
+	if err = client.SendServerCommand(server.Identifier, "say \"hello world\""); err != nil {
+		handleError(err)
+		return
+	}
+}
+
+func handleError(err error) {
+	if errs, ok := err.(*croc.ApiError); ok {
+		for _, e := range errs.Errors {
+			fmt.Println(e.Error())
+		}
+	} else {
+		fmt.Println(err.Error())
+	}
+}
